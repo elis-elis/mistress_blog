2923f07e48f10d11ddec6d7c6eccff2798aa2a1e
elis-elis <elisprostotak@gmail.com>
2024-08-06 22:15:05 +0200

    Added '/add' route in app.py, created add.html file and separate style_add.css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -21,6 +21,9 @@ def save_posts(posts):
 
 # CRUD Operations
 def add_post(author, title, content):
+    """
+    This function is responsible for adding the new blog post to the list of posts (and saving it to the JSON file).
+    """
     posts = load_posts()  # Load the current list of posts from the JSON file
     if not posts:
         new_id = 1
@@ -39,9 +42,22 @@ def index():  # sends the list of blog posts to a template for display
     return render_template('index.html', posts=blog_posts)
 
 
-@app.route('/add-post')
-def add_post():
-    pass
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # The request.form object is a dictionary that contains the form data.
+        author = request.form['author']
+        title = request.form['title']
+        content = request.form['content']
+        add_post(author, title, content)
+        return redirect(url_for('index'))
+        # After the new post is added, this line redirects the user to the index page (/).
+        # The redirect function is used to send the user to a different URL,
+        # and url_for('index') generates the URL for the index function (which shows the list of all blog posts).
+    return render_template('add.html')
+    # If the request method is not POST (which means it's a GET request, the default method when you visit a webpage),
+    # this line renders the add.html template.
+    # Rendering a template means creating an HTML page based on the template and sending it to the user's browser.
 
 
 if __name__ == '__main__':
